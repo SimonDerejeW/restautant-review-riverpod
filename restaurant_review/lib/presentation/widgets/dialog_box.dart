1de0a946564be22566a3983d5052c8e86f180c6c
iamsamienda <samuelendale7373@gmail.com>
2024-05-29 17:52:00 +0300

    chore: Refactor DialogBox to use StatefulWidget and add initialOpinion parameter

--- restaurant_review/lib/presentation/widgets/dialog_box.dart
+++ restaurant_review/lib/presentation/widgets/dialog_box.dart
@@ -1,47 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_review/presentation/widgets/my_button.dart';
-// import 'package:todo_app/components/my_button.dart';
-class DialogBox extends StatelessWidget {
-  // final controller;
-  // VoidCallback onSave;
-  VoidCallback onCancel;
 
+class DialogBox extends StatefulWidget {
+  final VoidCallback onCancel;
+  final void Function(String newOpinion) onSubmit;
+  final String initialOpinion;
 
-  DialogBox({super.key , required this.onCancel});
- 
+  const DialogBox({
+    super.key,
+    required this.onCancel,
+    required this.onSubmit,
+    required this.initialOpinion,
+  });
+
+  @override
+  _DialogBoxState createState() => _DialogBoxState();
+}
+
+class _DialogBoxState extends State<DialogBox> {
+  late TextEditingController _opinionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _opinionController = TextEditingController(text: widget.initialOpinion);
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Color.fromRGBO(235, 125, 56,1),
-      content: 
-          Container(
-            height: 150,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextField(
-                 
-                  decoration: InputDecoration(
-                    hintText: "Write a review",
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.amber)
-                    ),
-                    
-                  ),
+      backgroundColor: const Color.fromRGBO(235, 125, 56, 1),
+      content: SizedBox(
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: _opinionController,
+              decoration: const InputDecoration(
+                hintText: "Write a review",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amber),
                 ),
-              
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MyDialogButton(text: "Save", onPressed: () {}),
-              SizedBox(width: 8,),
-              MyDialogButton(text: "Cancel", onPressed: onCancel),
-            ],
-          )]
-        
-    )
-    )
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyDialogButton(
+                  text: "Save",
+                  onPressed: () {
+                    widget.onSubmit(_opinionController.text);
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                ),
+                const SizedBox(width: 8),
+                MyDialogButton(
+                  text: "Cancel",
+                  onPressed: widget.onCancel,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _opinionController.dispose();
+    print("disposed");
+    super.dispose();
   }
 }
