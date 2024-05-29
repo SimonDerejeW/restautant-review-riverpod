0600ed69c0859aaf0da4eea4492ffd9abd5f6e1f
iamsamienda <samuelendale7373@gmail.com>
2024-05-29 17:56:15 +0300

    feat: Add CommentService for managing comment actions

---
+++ restaurant_review/lib/infrastructure/comment/comment_service.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:restaurant_review/core/storage.dart';
+
+class CommentService {
+  final String baseUrl = 'http://localhost:3000/comment';
+  final SecureStorage _secureStorage = SecureStorage.instance;
+
+  CommentService();
+
+  Future<http.Response> createComment(
+      String restaurantId, String opinion) async {
+    String? token = await _secureStorage.read('token');
+    final url = Uri.parse(baseUrl);
+
+    print("Restauranid $restaurantId, opionion $opinion, baseurl $baseUrl");
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({
+        'restaurantId': restaurantId,
+        'opinion': opinion,
+      }),
+    );
+
+    return response;
+  }
+
+  Future<http.Response> updateComment(
+      String commentId, String newOpinion) async {
+    String? token = await _secureStorage.read('token');
+    final url = Uri.parse('$baseUrl/$commentId');
+    print(url);
+    final response = await http.put(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({
+        'opinion': newOpinion,
+      }),
+    );
+    print(response);
+    return response;
+  }
+
+  Future<http.Response> deleteComment(String commentId) async {
+    String? token = await _secureStorage.read('token');
+    final url = Uri.parse('$baseUrl/$commentId');
+    final response = await http.delete(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    return response;
+  }
+}
