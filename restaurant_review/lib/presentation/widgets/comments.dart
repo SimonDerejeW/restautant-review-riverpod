cf731e66ea14687fffaf2994d0cd3725a6787549
iamsamienda <samuelendale7373@gmail.com>
2024-05-29 17:55:17 +0300

    feat: Update Comment widget to display user info and comment text

--- restaurant_review/lib/presentation/widgets/comments.dart
+++ restaurant_review/lib/presentation/widgets/comments.dart
@@ -4,16 +4,18 @@ class Comment extends StatelessWidget {
   final Widget user_info;
   final String comment;
 
-  Comment({required this.user_info, required this.comment});
+  const Comment({super.key, required this.user_info, required this.comment});
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 20),
+      margin: const EdgeInsets.only(bottom: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           user_info,
           Text(comment),
         ],
+
       ),
     );
   }
