769ab87f553aafa4db1c88a070def5242827765e
Simon <simondereje1@gmail.com>
2024-05-31 04:17:10 +0300

    feat: Add restaurant creation, update, and deletion functionality
    This commit introduces code changes to enable the creation, update, and deletion of restaurants in the application. It adds new classes `RestaurantCreationLoading`, `RestaurantCreated`, `RestaurantUpdateLoading`, `RestaurantUpdated`, `RestaurantDeleteLoading`, and `RestaurantDeleted` to the `RestaurantState` file. These classes handle the loading and success states for restaurant creation, update, and deletion operations. The `RestaurantCreated` and `RestaurantUpdated` classes include parameters to store the created or updated restaurant details respectively. This feature enhances the application by allowing users to perform CRUD operations on restaurants.

--- restaurant_review/lib/application/retaurant/restaurant_state.dart
+++ restaurant_review/lib/application/retaurant/restaurant_state.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
+import 'package:restaurant_review/infrastructure/restaurant/create_restaurant_dto.dart';
 import 'package:restaurant_review/infrastructure/restaurant/restaurant_dto.dart';
+import 'package:restaurant_review/infrastructure/restaurant/update_restaurant_dto.dart';
 
 abstract class RestaurantState extends Equatable {
   @override
@@ -27,6 +29,32 @@ class RestaurantDetailLoaded extends RestaurantState {
   @override
   List<Object?> get props => [restaurant];
 }
+
+class RestaurantCreationLoading extends RestaurantState {}
+
+class RestaurantCreated extends RestaurantState {
+  final CreateRestaurantDTO restaurant;
+
+  RestaurantCreated(this.restaurant);
+
+  @override
+  List<Object?> get props => [restaurant];
+}
+
+class RestaurantUpdateLoading extends RestaurantState {}
+
+class RestaurantUpdated extends RestaurantState {
+  final UpdateRestaurantDTO restaurant;
+
+  RestaurantUpdated(this.restaurant);
+
+  @override
+  List<Object?> get props => [restaurant];
+}
+
+class RestaurantDeleteLoading extends RestaurantState {}
+
+class RestaurantDeleted extends RestaurantState {}
 
 class RestaurantError extends RestaurantState {
   final String message;
